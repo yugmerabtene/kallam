d83973fbeca5fe933979727876facdeb6625d519
yugmerabtene <yug.merabtene@gmail.com>
2026-03-11 01:12:53 +0100

    fix: use STORAGES dict (Django 6.0) instead of deprecated STATICFILES_STORAGE for whitenoise manifest storage

--- kallam/settings.py
+++ kallam/settings.py
@@ -174,7 +174,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(os.getenv('DJANGO_MEDIA_ROOT', str(BASE_DIR / 'media')))
 
